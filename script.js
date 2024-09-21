1ee4643157eb9c635a006a26196416de076f8c66
vjanja05 <166657210+vjanja05@users.noreply.github.com>
2024-09-21 12:21:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+jQuery(function(){
+    jQuery('#showall').click(function(){
+    jQuery('.target').show();
+});
+jQuery('.single').click(function(){
+    jQuery('.target').hide();
+    jQuery('#div'+$(this).attr('target')).show();
+});
+});
